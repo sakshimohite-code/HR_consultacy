ecb0ea23bd1b6af9b6e7123bb200c4a9e1a627f8
Sakshi Mohite <sakshimohite206@gmail.com>
2026-01-20 10:31:38 +0530

    Update MySQL connection details

--- conn.js
+++ conn.js
@@ -2,10 +2,10 @@ var mysql = require('mysql2');
 var util = require('util');
 
 var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'hr_consultancy'
+    host: 'bjvh9ankxjic2er5su2a-mysql.services.clever-cloud.comt',
+    user: 'udei2noavssvzlhs',
+    password: 'HbB1rOFUIWSqyDc5El5F',
+    database: 'bjvh9ankxjic2er5su2a'
 });
 var exe = util.promisify(conn.query).bind(conn);
 
